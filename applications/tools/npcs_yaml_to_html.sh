3a57b3f640bdb3f0147858551cb5d11685653362
Johann Tienhaara <jtienhaara@yahoo.com>
2021-06-10 06:53:55 -0300

    Add a summary HTML table with all data from all NPCs...  for now, at least.

--- applications/tools/npcs_yaml_to_html.sh
+++ applications/tools/npcs_yaml_to_html.sh
@@ -21,6 +21,10 @@ RUN_DIR=`dirname $0`
 NPCS_DIR="$CONTENT_DIR/npcs"
 HTML_OUTPUT_DIR="$NPCS_DIR/html"
 HTML_NPC_TEMPLATE="$HTML_OUTPUT_DIR/npc_template.html"
+HTML_ALL_NPCS_TEMPLATE="$HTML_OUTPUT_DIR/npcs_all_template.html"
+HTML_ALL_NPCS_TEMPLATE_PRE="$HTML_OUTPUT_DIR/npcs_all_template_pre.html"
+HTML_ALL_NPCS_TEMPLATE_POST="$HTML_OUTPUT_DIR/npcs_all_template_post.html"
+HTML_ALL_NPCS_SUMMARY="$HTML_OUTPUT_DIR/npcs_all_summary.html"
 
 #
 # First validate all the YAML files.
@@ -29,10 +33,28 @@ HTML_NPC_TEMPLATE="$HTML_OUTPUT_DIR/npc_template.html"
     || exit 1
 
 #
+# Create the big giant summary HTML table.
+#
+cat "$HTML_ALL_NPCS_TEMPLATE_PRE" > "$HTML_ALL_NPCS_SUMMARY" \
+    || exit 2
+for NPC_YAML_FILE in $NPCS_DIR/*.yaml
+do
+    "$RUN_DIR/yaml_to_html.sh" "$HTML_ALL_NPCS_TEMPLATE" "$NPC_YAML_FILE" \
+        || exit 3
+    HTML_NPC_OUTPUT_FILE_BASE=`echo "$NPC_YAML_FILE" \
+                                   | sed 's|^.*/\([^/]*\)\.yaml$|\1|'`
+    HTML_NPC_OUTPUT_FILE="$HTML_OUTPUT_DIR/$HTML_NPC_OUTPUT_FILE_BASE.html"
+    cat "$HTML_NPC_OUTPUT_FILE" >> "$HTML_ALL_NPCS_SUMMARY" \
+        || exit 4
+done
+cat "$HTML_ALL_NPCS_TEMPLATE_POST" >> "$HTML_ALL_NPCS_SUMMARY" \
+    || exit 5
+
+#
 # OK, now we can convert them all into HTML.
 #
 "$RUN_DIR/yaml_to_html.sh" "$HTML_NPC_TEMPLATE" $NPCS_DIR/*.yaml \
-    || exit 2
+    || exit 6
 
 echo ""
 echo "SUCCESS converting NPCs from YAML to HTML: $HTML_OUTPUT_DIR"
