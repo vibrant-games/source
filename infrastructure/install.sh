a8e79634022a5536388027e998d49d3364f44f09
Johann Tienhaara <jtienhaara@yahoo.com>
2021-10-17 22:12:03 -0300

    Switch from VM to Kubernetes.

--- infrastructure/install.sh
+++ infrastructure/install.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+DATE_TIME=`date '+%Y%m%d%H%M%S'`
+
 USER_NAME=`id --user --name`
 GROUP_NAME=`id --user --name`
+
+RUN_DIR=`dirname $0`
+if test "$RUN_DIR" = ""
+then
+    RUN_DIR=`pwd`
+fi
+
+LOG_FILE="$RUN_DIR/${DATE_TIME}_infrastructure.log"
 
 sudo docker run --mount type=bind,readonly=false,source=`pwd`,destination=/vibrant_games/terraform hashicorp/terraform:1.0.8@sha256:c4abe7a8d7b4ae05da852d56093da3704d6684a6d560d31e3a3ab18335cf5dcf -chdir=/vibrant_games/terraform init \
     || exit 1
@@ -16,7 +26,14 @@ sudo chown -R "$USER_NAME:$GROUP_NAME" . \
 # Add --env TF_LOG=DEBUG to turn debug logging on:
 #
 sudo docker run --mount type=bind,readonly=false,source=`pwd`,destination=/vibrant_games/infrastructure --mount type=bind,readonly=true,source=$HOME/.ssh/vibrant_games,destination=/root/.ssh/vibrant_games --env TF_VAR_do_personal_access_token=`cat ../../scrts/do_terraform_key.txt | grep -v '^$'` --env TF_VAR_do_ssh_private_key_file=/root/.ssh/vibrant_games/do_id_ed25519 hashicorp/terraform:1.0.8@sha256:c4abe7a8d7b4ae05da852d56093da3704d6684a6d560d31e3a3ab18335cf5dcf -chdir=/vibrant_games/infrastructure apply /vibrant_games/infrastructure/tfplan \
+     2>&1 \
+    | tee "$LOG_FILE" \
     || exit 4
+EXIT_CODE=$?
+if test $EXIT_CODE -ne 0
+then
+    exit 4
+fi
 
 sudo chown -R "$USER_NAME:$GROUP_NAME" . \
     || exit 5
--- infrastructure/production.tf
+++ infrastructure/production.tf
@@ -18,67 +18,144 @@ data "digitalocean_ssh_key" "do_terraform" {
   name = "jtienhaara@yahoo.com"
 }
 
+# !!! #
+# !!! # For now just a single webserver VM.
+# !!! # (Eventually Kubernetes cluster with Istio etc.)
+# !!! #
+# !!! resource "digitalocean_droplet" "web" {
+# !!!   image = "ubuntu-20-04-x64"
+# !!!   name = "web"
+# !!!   region = "tor1"
+# !!!   size = "s-1vcpu-1gb"
+# !!!   ssh_keys = [
+# !!!     data.digitalocean_ssh_key.do_terraform.id
+# !!!   ]
+# !!! 
+# !!!   connection {
+# !!!     host = self.ipv4_address
+# !!!     user = "root"
+# !!!     type = "ssh"
+# !!!     private_key = file(var.do_ssh_private_key_file)
+# !!!     timeout = "2m"
+# !!!   }
+# !!! 
+# !!!   provisioner "remote-exec" {
+# !!!     inline = [
+# !!!       "export PATH=$PATH:/usr/bin",
+# !!!       # Install nginx:
+# !!!       "sudo apt update",
+# !!!       "sudo apt install -y nginx"
+# !!!     ]
+# !!!   }
+# !!! }
+
+
 #
-# For now just a single webserver VM.
-# (Eventually Kubernetes cluster with Istio etc.)
+# DNS currently done by hand :(
+# Need to re-jig the domain stuff to use Terraform,
+# and move all of the email etc domain records into here.
 #
-resource "digitalocean_droplet" "web" {
-  image = "ubuntu-20-04-x64"
-  name = "web"
-  region = "tor1"
-  size = "s-1vcpu-1gb"
-  ssh_keys = [
-    data.digitalocean_ssh_key.do_terraform.id
-  ]
+# !!! resource "digitalocean_domain" "vibrantgames_ca" {
+# !!!   name = "vibrantgames.ca"
+# !!!   # !!! ip_address = digitalocean_loadbalancer.web.iv4_address
+# !!! }
 
-  connection {
-    host = self.ipv4_address
-    user = "root"
-    type = "ssh"
-    private_key = file(var.do_ssh_private_key_file)
-    timeout = "2m"
+# NEED IP ADDRESS:
+# !!! resource "digitalocean_record" "www_vibrantgames_ca" {
+# !!!   # !!! domain = digitalocean_domain.vibrantgames_ca.name
+# !!!   domain = "vibrantgames.ca"
+# !!!   type = "A"
+# !!!   name = "www"
+# !!!   value = !!!ip address!!!
+# !!! }
+
+# !!! resource "digitalocean_certificate" "certificate_production" {
+# !!!   # !!! name = "production-2021-10-10"
+# !!!   name = "certificate-production"
+# !!!   type = "lets_encrypt"
+# !!!   domains = [ digitalocean_domain.www_vibrantgames_ca.name ]
+# !!! }
+
+# !!! resource "digitalocean_loadbalancer" "web" {
+# !!!   name = "web-load-balancer"
+# !!!   region = "tor1"
+# !!! 
+# !!!   forwarding_rule {
+# !!!     entry_port = 443
+# !!!     entry_protocol = "https"
+# !!! 
+# !!!     target_port = 80
+# !!!     target_protocol = "http"
+# !!! 
+# !!!     certificate_name = digitalocean_certificate.certificate_production.name
+# !!!   }
+# !!! 
+# !!!   healthcheck {
+# !!!     port = 22
+# !!!     protocol = "tcp"
+# !!!   }
+# !!! 
+# !!!   droplet_ids = [ digitalocean_droplet.web.id ]
+# !!! }
+
+
+
+#
+# Adapted from https://github.com/ponderosa-io/tf-digital-ocean-cluster/blob/master/digital-ocean-cluster.tf
+#
+
+data "digitalocean_kubernetes_versions" "version" {
+  #
+  # At the time of writing, 1.21.3-do.0 is available.
+  #
+  version_prefix = "1.21."
+}
+
+resource "digitalocean_kubernetes_cluster" "production" {
+  name    = "production"
+  region  = "tor1"
+
+  version = data.digitalocean_kubernetes_versions.version.latest_version
+  auto_upgrade = true
+
+  #
+  # High availability control plane:
+  # NOT supported in tor1 data centre
+  #
+  # ha = true
+
+  # !!! tags
+
+  maintenance_policy {
+    day = "thursday"
+
+    # UTC.  EST=UTC-5, EDT=UTC-4.
+    # So 6:00 = 1am-5am winter, 2am-6am summer Eastern time.
+    start_time = "6:00"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      # Install nginx:
-      "sudo apt update",
-      "sudo apt install -y nginx"
-    ]
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+    # !!! auto_scale = true
+    # !!! min_nodes = 1
+    # !!! max_nodes = 5
+    # !!! tags
+    # !!! labels
   }
 }
 
-
-resource "digitalocean_domain" "www_vibrant_games_ca" {
-  name = "www.vibrantgames.ca"
-  # !!! ip_address = digitalocean_loadbalancer.web.iv4_address
+#
+# Container registry for private images:
+#
+resource "digitalocean_container_registry" "vibrantgames_production_registry" {
+  name = "vibrantgames-production-registry"
+  subscription_tier_slug = "starter"
+  # !!! endpoint = "registry.digitalocean.com/myregistry"
+  # !!! server_url = "registry.digitalocean.com"
 }
 
-resource "digitalocean_certificate" "certificate_production" {
-  name = "production-2021-10-10"
-  type = "lets_encrypt"
-  domains = [ digitalocean_domain.www_vibrant_games_ca.name ]
-}
-
-resource "digitalocean_loadbalancer" "web" {
-  name = "web-load-balancer"
-  region = "tor1"
-
-  forwarding_rule {
-    entry_port = 443
-    entry_protocol = "https"
-
-    target_port = 80
-    target_protocol = "http"
-
-    certificate_name = digitalocean_certificate.certificate_production.name
-  }
-
-  healthcheck {
-    port = 22
-    protocol = "tcp"
-  }
-
-  droplet_ids = [ digitalocean_droplet.web.id ]
+output "cluster-id" {
+  value = "${digitalocean_kubernetes_cluster.production.id}"
 }
