ab01363e65abf303a5491a778c3200670ed001b9
Johann Tienhaara <jtienhaara@yahoo.com>
2021-10-19 12:21:19 -0300

    Forgot to tag the container image when publishing new version of www server.  Old version kept getting pushed to cloud.

--- deep/publish_www.sh
+++ deep/publish_www.sh
@@ -9,19 +9,22 @@ cd ~/development/deep/ \
     || exit 2
 
 sudo doctl auth switch --context vibrantgames \
-    || exit 4
-
-sudo doctl registry login \
-    || exit 5
-
-configured_infrastructure_compute=kind configured_infrastructure_registry=digitalocean_container_registry configured_config_web_www_content=git@github.com:vibrant-games/docs-index.git ./application/web/www/nginx/install.sh --nowait \
     || exit 3
 
-sudo docker push registry.digitalocean.com/production-registry/www:0.0.1 \
+sudo doctl registry login \
+    || exit 4
+
+configured_infrastructure_compute=kind configured_infrastructure_registry=digitalocean_container_registry configured_config_web_www_content=git@github.com:vibrant-games/docs-index.git ./application/web/www/nginx/install.sh --nowait \
+    || exit 5
+
+sudo docker tag localhost/www:0.0.1 registry.digitalocean.com/production-registry/www:0.0.1 \
     || exit 6
 
-kubectl rollout restart deployment www-v1 --namespace www --kubeconfig ~/development/vibrant_games/source/infrastructure/kubeconfig.json \
+sudo docker push registry.digitalocean.com/production-registry/www:0.0.1 \
     || exit 7
+
+kubectl rollout restart deployment www-v1 --namespace www --kubeconfig ~/development/vibrant_games/source/infrastructure/kubeconfig.json \
+    || exit 8
 
 echo "SUCCESS publishing www."
 
